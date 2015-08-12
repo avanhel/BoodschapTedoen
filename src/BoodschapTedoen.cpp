1e73282ef5584ab204a0f7095d2f514f681f046a
Arnaud van Helden <avanhelden@gmail.com>
2015-08-12 21:47:26 +0200

    Eerste opzet van Boodschap te doen.

---
+++ src/BoodschapTedoen.cpp
@@ -0,0 +1,20 @@
+/*
+ * BoodschapTedoen.cpp
+ *
+ *  Created on: 28 Jul 2015
+ *      Author: avanhelden
+ */
+
+#include "BoodschapTedoen.h"
+
+BoodschapTedoen::BoodschapTedoen()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+BoodschapTedoen::~BoodschapTedoen()
+{
+	// TODO Auto-generated destructor stub
+}
+
